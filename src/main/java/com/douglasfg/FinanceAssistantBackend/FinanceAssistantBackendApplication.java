9e57c5b24b93357ed7f15cc600b9cc5131ba57d7
douglasferreirag <douglas.ferreirag@gmail.com>
2026-06-16 20:12:50 -0300

    Inicial Commit

---
+++ src/main/java/com/douglasfg/FinanceAssistantBackend/FinanceAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.douglasfg.FinanceAssistantBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglasfg/FinanceAssistantBackend/FinanceAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasfg.FinanceAssistantBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
